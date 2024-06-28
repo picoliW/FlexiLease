097f976451099439800d596169fce7fdcf421c4a
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-28 20:37:23 -0300

    feat: create car route in shared infra

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,9 @@
+import carsRouter from "@modules/cars/infra/http/routes/Car.route";
 import { Router } from "express";
 
 const routes = Router();
+
+routes.use("/car", carsRouter);
 
 routes.get("/", (request, response) => {
   return response.json("Hello world");
