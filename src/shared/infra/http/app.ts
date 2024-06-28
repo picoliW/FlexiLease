dc2f85fe4c37495a4fd21341896d7351d8e8b32d
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-28 20:37:56 -0300

    feat: use errors from celebrate to capture errors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,7 @@
+import "reflect-metadata";
+import "@shared/container";
+import { errors } from "celebrate";
+import cors from "cors";
 import express from "express";
 import routes from "./routes";
 import { ErrorHandlerMiddleware } from "./middlewares/ErrorHandler";
@@ -7,9 +11,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/api/v1", routes);
 
+app.use(errors());
 app.use(ErrorHandlerMiddleware);
 app.use(NotFound);
 
