64c86d48c21c13acebcd7332688a64e8925ff8bc
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-28 17:30:07 -0300

    fix: put /api/v1 to be main route

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use("/api/v1", routes);
 
 app.use(ErrorHandlerMiddleware);
 app.use(NotFound);
