5025702100681832b219404595ffda6f7753a2e2
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-29 11:39:30 -0300

    feat: create middleware to validate ObjectId

---
+++ src/shared/infra/http/middlewares/ValidateDeleteCar.ts
@@ -0,0 +1,14 @@
+import { ValidateObjectId } from "@modules/cars/infra/schemas/ValidateObjectIdSchema";
+import { NextFunction, Request, Response } from "express";
+
+export const validateObjectIdMIddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { error } = ValidateObjectId.validate(req.params);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
