f25b9f26f86ce0cc08f6be7b9c0c2c65df455be7
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-29 16:50:15 -0300

    feat: add base route to users

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,12 @@
 import carsRouter from "@modules/cars/infra/http/routes/Car.route";
+import usersRouter from "@modules/users/infra/http/routes/User.route";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.use("/car", carsRouter);
+
+routes.use("/user", usersRouter);
 
 routes.get("/", (request, response) => {
   return response.json("Hello world");
