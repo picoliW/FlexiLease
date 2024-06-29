144caced0e13eb4e0981c10749b6966aea8b116d
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-29 11:36:07 -0300

    feat: create update route

--- src/modules/cars/infra/http/routes/Car.route.ts
+++ src/modules/cars/infra/http/routes/Car.route.ts
@@ -1,25 +1,21 @@
 import { container } from "tsyringe";
 import { Router } from "express";
 import { CarController } from "../controllers/CarController";
-import { createCarSchema } from "../../schemas/CreateCarSchema";
-import { DeleteCarSchema } from "../../schemas/DeleteCarSchema";
+import { CreateCarSchema } from "../../schemas/CreateCarSchema";
+import { UpdateCarSchema } from "../../schemas/UpdateCarSchema";
+import { validateObjectIdMIddleware } from "@shared/infra/http/middlewares/ValidateDeleteCar";
 
 const carsRouter = Router();
 const carsController = container.resolve(CarController);
 
-carsRouter.post("/", createCarSchema, carsController.create);
+carsRouter.post("/", CreateCarSchema, carsController.create);
 carsRouter.get("/", carsController.index);
-
-carsRouter.delete(
+carsRouter.delete("/:id", validateObjectIdMIddleware, carsController.delete);
+carsRouter.put(
   "/:id",
-  (req, res, next) => {
-    const { error } = DeleteCarSchema.validate(req.params);
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
-    }
-    next();
-  },
-  carsController.delete,
+  UpdateCarSchema,
+  validateObjectIdMIddleware,
+  carsController.update,
 );
 
 export default carsRouter;
