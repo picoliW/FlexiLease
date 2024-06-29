e20b462858d8532b874dfadb9a2a7a840ffaf96f
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-29 17:40:55 -0300

    feat: create index method in user controller

--- src/modules/users/infra/http/controller/UserController.ts
+++ src/modules/users/infra/http/controller/UserController.ts
@@ -2,8 +2,17 @@ import { Request, Response } from "express";
 import { instanceToInstance } from "class-transformer";
 import CreateUserService from "@modules/users/services/CreateUserService";
 import { container } from "tsyringe";
+import ListUserService from "@modules/users/services/ListUserService";
 
 export default class UsersController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const listUser = container.resolve(ListUserService);
+
+    const users = await listUser.execute();
+
+    return res.json(instanceToInstance(users));
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, cpf, birth, email, password, cep, qualified } = request.body;
 
