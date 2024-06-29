ffb91638501efea774959138263818cf3936d038
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-29 01:03:17 -0300

    feat: create delete route

--- src/modules/cars/infra/http/routes/Car.route.ts
+++ src/modules/cars/infra/http/routes/Car.route.ts
@@ -2,11 +2,24 @@ import { container } from "tsyringe";
 import { Router } from "express";
 import { CarController } from "../controllers/CarController";
 import { createCarSchema } from "../../schemas/CreateCarSchema";
+import { DeleteCarSchema } from "../../schemas/DeleteCarSchema";
 
 const carsRouter = Router();
 const carsController = container.resolve(CarController);
 
 carsRouter.post("/", createCarSchema, carsController.create);
 carsRouter.get("/", carsController.index);
+
+carsRouter.delete(
+  "/:id",
+  (req, res, next) => {
+    const { error } = DeleteCarSchema.validate(req.params);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+    next();
+  },
+  carsController.delete,
+);
 
 export default carsRouter;
