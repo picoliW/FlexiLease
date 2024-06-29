ee84388e59fda175346fd51d09109729bea09663
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-29 14:06:04 -0300

    feat: add id to the accessory

--- src/modules/cars/infra/typeorm/entities/dtos/Acessory.ts
+++ src/modules/cars/infra/typeorm/entities/dtos/Acessory.ts
@@ -1,6 +1,12 @@
+import { Exclude } from "class-transformer";
 import { IsString } from "class-validator";
+import { ObjectId } from "mongodb";
 
 export class Accessory {
+  @IsString()
+  @Exclude()
+  id: ObjectId;
+
   @IsString()
   description: string;
 }
