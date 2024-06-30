f5e778095acb6bdddcf04ea1587280fba2462f98
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-30 15:05:02 -0300

    feat: create route to show one reserve

--- src/modules/reserve/infra/http/routes/reserve.route.ts
+++ src/modules/reserve/infra/http/routes/reserve.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { container } from "tsyringe";
 import ReserveController from "../controller/ReserveController";
 import { validateReserve } from "../../schemas/CreateReserveValidation";
+import { validateObjectIdMIddleware } from "@shared/infra/http/middlewares/ValidateDeleteCar";
 
 const reserveRouter = Router();
 const reserveController = container.resolve(ReserveController);
 
 reserveRouter.post("/", validateReserve, reserveController.create);
 reserveRouter.get("/", reserveController.index);
+reserveRouter.get("/:id", validateObjectIdMIddleware, reserveController.show);
 
 export default reserveRouter;
