932dd99620ad7c3a1eebfd4c0ee9702f8845862f
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-29 21:44:02 -0300

    feat: create function to calculate user age

---
+++ src/shared/utils/dateUtils.ts
@@ -0,0 +1,13 @@
+export function calculateAge(birthDate: Date): number {
+  const today = new Date();
+  const birth = new Date(birthDate);
+
+  let age = today.getFullYear() - birth.getFullYear();
+  const m = today.getMonth() - birth.getMonth();
+
+  if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+    age--;
+  }
+
+  return age;
+}
