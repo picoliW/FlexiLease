e1a6b5c894850337a48bdb71ae3d36983b7cd53a
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-30 11:58:37 -0300

    feat: create schemas to validate reserve creation

---
+++ src/modules/reserve/infra/schemas/CreateReserveValidation.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { CreateReserveSchema } from "./CreateReserveSchema";
+
+export const validateReserve = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { error } = CreateReserveSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
