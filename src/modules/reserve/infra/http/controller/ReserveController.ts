9da4ae2e5f48d6d23c36a0b9d11cb1558db4f7f9
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-30 12:34:09 -0300

    feat: add error handlers to reserve controller

--- src/modules/reserve/infra/http/controller/ReserveController.ts
+++ src/modules/reserve/infra/http/controller/ReserveController.ts
@@ -2,20 +2,34 @@ import { Request, Response } from "express";
 import { instanceToInstance } from "class-transformer";
 import { container } from "tsyringe";
 import CreateReserveService from "@modules/reserve/services/CreateReserveService";
+import { ConflictError } from "@shared/errors/ConflictError";
+import { NotFoundError } from "@shared/errors/NotFoundError";
+import { BadRequestError } from "@shared/errors/BadRequestError";
 
 export default class ReserveController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const { id_user, start_date, end_date, id_car } = request.body;
+    try {
+      const { id_user, start_date, end_date, id_car } = request.body;
 
-    const createReserve = container.resolve(CreateReserveService);
+      const createReserve = container.resolve(CreateReserveService);
 
-    const reserve = await createReserve.execute({
-      id_user,
-      start_date,
-      end_date,
-      id_car,
-    });
+      const reserve = await createReserve.execute({
+        id_user,
+        start_date,
+        end_date,
+        id_car,
+      });
 
-    return response.json(instanceToInstance(reserve));
+      return response.json(instanceToInstance(reserve));
+    } catch (error) {
+      if (error instanceof ConflictError) {
+        return response.status(409).json({ message: error.message });
+      } else if (error instanceof BadRequestError) {
+        return response.status(400).json({ message: error.message });
+      } else if (error instanceof NotFoundError) {
+        return response.status(400).json({ message: error.message });
+      }
+      throw error;
+    }
   }
 }
