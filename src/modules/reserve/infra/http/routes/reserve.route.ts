704daa3ffa5c1af180a323eb774cf218ce4cb1f1
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-30 11:58:11 -0300

    feat: post route to create reserve

---
+++ src/modules/reserve/infra/http/routes/reserve.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { container } from "tsyringe";
+import ReserveController from "../controller/ReserveController";
+import { validateReserve } from "../../schemas/CreateReserveValidation";
+
+const reserveRouter = Router();
+const reserveController = container.resolve(ReserveController);
+
+reserveRouter.post("/", validateReserve, reserveController.create);
+
+export default reserveRouter;
