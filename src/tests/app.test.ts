67b393d6973b45794abb0024b6413d4d78819551
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-07-03 14:40:27 -0300

    test: create test to test default route

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { app } from "@shared/infra/http/app";
 
-test("Deve responder na raiz", () => {
+test("Should respond at the root ", () => {
   return request(app).get("/api/v1").expect(200);
 });
