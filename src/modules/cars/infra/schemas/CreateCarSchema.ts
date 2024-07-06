ee6d88a6593916f09eb6c2ea52975c110f6d73bf
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-07-06 01:09:22 -0300

    docs create cars swagger components

--- src/modules/cars/infra/schemas/CreateCarSchema.ts
+++ src/modules/cars/infra/schemas/CreateCarSchema.ts
@@ -1,5 +1,71 @@
 import { Joi, celebrate } from "celebrate";
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     CreateCar:
+ *       type: object
+ *       required:
+ *         - model
+ *         - color
+ *         - year
+ *         - value_per_day
+ *         - accessories
+ *         - number_of_passengers
+ *       properties:
+ *         model:
+ *           type: string
+ *           default: GM S10 2.8
+ *         color:
+ *           type: string
+ *           default: White
+ *         year:
+ *           type: string
+ *           default: "2021"
+ *         value_per_day:
+ *           type: number
+ *           default: 50
+ *         accessories:
+ *           type: array
+ *           default:
+ *             - { description: "Air conditioner" }
+ *             - { description: "4x4 traction" }
+ *             - { description: "4 ports" }
+ *           items:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *             required:
+ *               - description
+ *           minItems: 1
+ *           uniqueItems: true
+ *         number_of_passengers:
+ *           type: number
+ *           default: 5
+ *     CreateCarResponse:
+ *       type: object
+ *       properties:
+ *         model:
+ *           type: string
+ *         color:
+ *           type: string
+ *         year:
+ *           type: string
+ *         value_per_day:
+ *           type: number
+ *         accessories:
+ *           type: array
+ *           items:
+ *             type: object
+ *         number_of_passengers:
+ *           type: number
+ *         _id:
+ *           type: string
+ *
+ */
+
 export const CreateCarSchema = celebrate({
   body: {
     model: Joi.string().required(),
